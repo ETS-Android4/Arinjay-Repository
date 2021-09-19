5a591a93031d7d55262948f675a4f9962337bbb5
arinjaybasak <arinjaybasak@gmail.com>
2021-09-19 15:35:12 -0700

    u

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArinjayDrivetrain.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp(name = "ArinjayDrivetrain", group = "LinearOpMode")
+
+public class ArinjayDrivetrain extends LinearOpMode {
+    //declare motors
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+
+    public void runOpMode() {
+        //initialize motors
+        leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
+        rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
+
+        waitForStart();
+
+        //set motors to controller input
+        while(opModeIsActive()) {
+            leftMotor.setPower(gamepad1.left_stick_y);
+            rightMotor.setPower(-1*gamepad1.right_stick_y);
+        }
+    }
+}
