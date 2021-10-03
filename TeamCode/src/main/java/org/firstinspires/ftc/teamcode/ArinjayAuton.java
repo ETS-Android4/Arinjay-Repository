a59f80348e7845fd9ee371427c026f11d6baf644
arinjaybasak <arinjaybasak@gmail.com>
2021-10-03 13:39:53 -0700

    Update ArinjayAuton.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArinjayAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArinjayAuton.java
@@ -20,7 +20,7 @@ public class ArinjayAuton extends LinearOpMode {
     static final double wheelDiameter = 3;
     static final double ticksPerRevolution = 537.6;
     double circumference = Math.PI * wheelDiameter;
-    public int direction[] = {0,1,2,3};
+    public int direction[] = {0,90,180,270,45,315,225,135};
     //test
 
 
@@ -31,48 +31,75 @@ public class ArinjayAuton extends LinearOpMode {
         backRight.setPower(speed);
     }
 
-    public void forward(int ticks) {//0
+    public void dirF(int ticks) {//0
         frontLeft.setTargetPosition(ticks);
         backLeft.setTargetPosition(ticks);
         frontRight.setTargetPosition(ticks);
         backRight.setTargetPosition(ticks);
     }
-    public void backward(int ticks) {//1
+    public void dirB(int ticks) {//1
         ticks*=-1;
         frontLeft.setTargetPosition(ticks);
         backLeft.setTargetPosition(ticks);
         frontRight.setTargetPosition(ticks);
         backRight.setTargetPosition(ticks);
     }
-    public void right(int ticks) {//2
+    public void dirR(int ticks) {//2
         frontLeft.setTargetPosition(ticks);
         backLeft.setTargetPosition(ticks * -1);
         frontRight.setTargetPosition(ticks * -1);
         backRight.setTargetPosition(ticks);
     }
-    public void left(int ticks) {//3
+    public void dirL(int ticks) {//3
         frontLeft.setTargetPosition(ticks * -1);
         backLeft.setTargetPosition(ticks);
         frontRight.setTargetPosition(ticks);
         backRight.setTargetPosition(ticks * -1);
     }
-
-
-
+    public void dirFR(int ticks) {//4
+        frontLeft.setTargetPosition(ticks);
+        backRight.setTargetPosition(ticks);
+    }
+    public void dirFL(int ticks) {//5
+        frontRight.setTargetPosition(ticks);
+        backLeft.setTargetPosition(ticks);
+    }
+    public void dirBL(int ticks) {//6
+        ticks*=-1;
+        frontLeft.setTargetPosition(ticks);
+        backRight.setTargetPosition(ticks);
+    }
+    public void dirBR(int ticks) {//7
+        ticks*=-1;
+        frontRight.setTargetPosition(ticks);
+        backLeft.setTargetPosition(ticks);
+    }
 
     public double move(int dir, double dist) { //dir{for,back,right,left}
         int ticks = (int)(dist*ticksPerRevolution/circumference);
-        if(dir == direction[0]) {
-            forward(ticks);
+        if((dir+360)%360 == direction[0]) {
+            dirF(ticks);
         }
-        else if(dir == direction[1]) {
-            backward(ticks);
+        else if((dir+360)%360 == direction[1]) {
+            dirB(ticks);
         }
-        else if(dir == direction[2]) {
-            right(ticks);
+        else if((dir+360)%360 == direction[2]) {
+            dirR(ticks);
         }
-        else if(dir == direction[3]) {
-            left(ticks);
+        else if((dir+360)%360 == direction[3]) {
+            dirL(ticks);
+        }
+        else if((dir+360)%360 == direction[4]) {
+            dirFR(ticks);
+        }
+        else if((dir+360)%360 == direction[5]) {
+            dirFL(ticks);
+        }
+        else if((dir+360)%360 == direction[6]) {
+            dirBL(ticks);
+        }
+        else if((dir+360)%360 == direction[7]) {
+            dirBR(ticks);
         }
         frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -98,6 +125,7 @@ public class ArinjayAuton extends LinearOpMode {
         waitForStart();
 
         //set motors to controller input
-
+        //something like move(<dir>,<dist>)
+        //dir{0,90,180,270,45,135,225,315} OR use negative angles for left
     }
 }
